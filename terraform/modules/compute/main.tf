b099a8ca1b6cd9eb20acddab4310b9c6b10a81e5
szuryuu <ilhamdzaky2007@gmail.com>
2026-01-14 11:39:08 +0700

    fix: version handler AMA

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_virtual_machine_extension" "ama" {
   virtual_machine_id   = azurerm_linux_virtual_machine.main.id
   publisher            = "Microsoft.Azure.Monitor"
   type                 = "AzureMonitorLinuxAgent"
-  type_handler_version = "1.38.0"
+  type_handler_version = "1.12"
 }
 
 resource "azurerm_monitor_data_collection_rule" "dcr" {
