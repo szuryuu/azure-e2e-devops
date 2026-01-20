dbc3567da2704cb009d9ce5fb7c4469f2edb6c57
szuryuu <ilhamdzaky2007@gmail.com>
2026-01-20 16:31:02 +0700

    fix: azure dashboard

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -9,7 +9,8 @@ resource "azurerm_portal_dashboard" "main" {
   }
 
   dashboard_properties = templatefile("${path.module}/dashboard.tpl", {
-    vm_id   = module.compute.vm_id
-    vm_name = module.compute.vm_name
+    md_content = "Variable content here!",
+    video_link = "https://www.youtube.com/watch?v=......",
+    sub_id     = data.azurerm_subscription.current.subscription_id
   })
 }
