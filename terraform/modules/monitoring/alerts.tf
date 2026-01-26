13ccbf7b1638594ab246842e73ed28a8a9a8433f
szuryuu <ilhamdzaky2007@gmail.com>
2026-01-26 09:38:44 +0700

    feat: alerts metic

--- terraform/modules/monitoring/alerts.tf
+++ terraform/modules/monitoring/alerts.tf
@@ -2,19 +2,26 @@ resource "azurerm_monitor_action_group" "main" {
   name                = "${var.project_name}-actiongroup"
   resource_group_name = var.resource_group_name
   short_name          = "e2eact"
+
+  email_receiver {
+    name          = "admin-alert"
+    email_address = "ilhamdzaky2007@gmail.com"
+    use_common_alert_schema = true
+  }
 }
 
-resource "azurerm_monitor_metric_alert" "main" {
-  name                = "${var.project_name}-metricalert"
+resource "azurerm_monitor_metric_alert" "vm_cpu" {
+  name                = "${var.project_name}-cpu-alert"
   resource_group_name = var.resource_group_name
   scopes              = var.vm_id
+  description         = "Alert if CPU > 20%"
 
   criteria {
     metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "Transactions"
-    aggregation      = "Total"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
     operator         = "GreaterThan"
-    threshold        = 10
+    threshold        = 20
   }
 
   action {
